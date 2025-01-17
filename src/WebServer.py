9c5877c95b8f41e997579e556ddc68ffdbf99dbe
CodeGoat <levijohnson2008@live.co.uk>
2025-01-17 16:55:16 +0000

    Chore: Bump firmware version
    Bumps the firmware version to v1.1.2.

--- src/WebServer.py
+++ src/WebServer.py
@@ -1,5 +1,5 @@
 # Goat - SecureMe WebServer class
-# Version 1.1.1
+# Version 1.1.2
 # © (c) 2024-2025 Goat Technologies
 # https://github.com/CodeGoat-dev/SecureMe
 # Description:
@@ -21,7 +21,7 @@ class WebServer:
     def __init__(self, ip_address="0.0.0.0", http_port=8000):
         """Constructs the class and exposes properties."""
         # Constants
-        self.VERSION = "1.1.1"
+        self.VERSION = "1.1.2"
 
         self.default_ip_address = "0.0.0.0"
         self.default_http_port = 8000
