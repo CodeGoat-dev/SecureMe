18be3bac5f7f0efcbd84095d35f8aa789a53a469
CodeGoat <levijohnson2008@live.co.uk>
2025-01-18 18:41:47 +0000

    Chore: Bump firmware version
    Bumps the firmware version to v1.1.5.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # Goat SecureMe
 # Lightweight, portable, Security System
-# Version 1.1.4
+# Version 1.1.5
 # © (c) 2024-2025 Goat Technologies
 # https://github.com/CodeGoat-dev/SecureMe
 # Description:
@@ -29,7 +29,7 @@ if utils.isPicoW():
     from GitHubUpdater import GitHubUpdater
 
 # Constants
-VERSION = "1.1.4"
+VERSION = "1.1.5"
 REPO_URL = "https://api.github.com/repos/CodeGoat-dev/SecureMe"
 
 # Pin constants
