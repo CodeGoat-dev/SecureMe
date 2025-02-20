57f881ec5eb5a409487a8683e9de5ef0ce387817
CodeGoat <levijohnson2008@live.co.uk>
2025-02-20 12:09:51 +0000

    Chore: Update build data
    Updates build data.

--- build/main.py
+++ build/main.py
@@ -1,9 +1,15 @@
-# Firmware entry point
+# Goat SecureMe
+# Firmware bootstrap utility
+# © (c) 2024-2025 Goat Technologies
+# https://github.com/CodeGoat-dev/SecureMe
+# Description:
+# Launches the Goat - SecureMe byte-compiled firmware.
+# Byte-compiled code cannot be automatically started so must be bootstrapped using a non-compiled main script.
 
 # Imports
 import machine
 
-# Try to import and run the SecureMe firmware
+# Try to import the compiled SecureMe firmware
 try:
     import SecureMe
 except Exception as e:
